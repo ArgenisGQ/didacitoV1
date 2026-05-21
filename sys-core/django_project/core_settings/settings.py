3fedae0f7d0aedf517b5bb78321c7aed7735beac
Argenis Gil <argenis.gq@gmail.com>
2026-05-20 23:49:57 -0400

    --

--- sys-core/django_project/core_settings/settings.py
+++ sys-core/django_project/core_settings/settings.py
@@ -64,6 +64,9 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD', 'password'),
         'HOST': os.getenv('DB_HOST', 'db'),
         'PORT': os.getenv('DB_PORT', '5432'),
+        'OPTIONS': {
+            'sslmode': os.getenv('DB_SSLMODE', 'disable'),
+        }
     }
 }
 
